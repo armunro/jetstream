b5d6f6bbd0fa860a95812fd63c599979700ddd58
Andrew Munro <12575339+armunro@users.noreply.github.com>
2022-07-13 12:25:35 -0600

    Starting database work

---
+++ Data/JetstreamContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Jetstream.Data;
+
+public class JetstreamContext : DbContext
+{
+    public DbSet<JetUnit> Unit { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseNpgsql(@"Host=postgres;Username=jetstream;Password=password;Database=jetstream");
+    
+}
+
+public class JetUnit
+{
+    public Guid Id { get; set; }
+    
+}
--- StatusModel.cs
+++
@@ -1,8 +0,0 @@
-namespace Jetstream;
-
-public class StatusModel
-{
-    public DateTime Date { get; set; }
-
-    public string? Summary { get; set; }
-}
