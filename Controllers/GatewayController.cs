acec173751966b4d4f8ffec621049227a6eb83bd
Andrew Munro <12575339+armunro@users.noreply.github.com>
2022-07-13 10:31:23 -0600

    Reorg

---
+++ Controllers/GatewayController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Jetstream.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class GatewayController : Controller
+{
+    // GET
+    [HttpPost(Name = "Create Unit")]
+    public object PostUnit([FromBody] object unit)
+    {
+        return new { Message = "Got it", Unit = unit};
+    }
+}
